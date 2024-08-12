94638d9de6317cd10d2b15b1582616afc2262496
raahuldatta <150013864+raahuldatta@users.noreply.github.com>
2024-08-12 22:49:57 +0530

    Add files via upload

---
+++ Task 4A/LogCommand.java
@@ -0,0 +1,14 @@
+public class LogCommand implements Command {
+    private LogHandler handler;
+    private LogLevel level;
+
+    public LogCommand(LogHandler handler, LogLevel level) {
+        this.handler = handler;
+        this.level = level;
+    }
+
+    @Override
+    public void execute(String message) {
+        handler.handleRequest(level, message);
+    }
+}
